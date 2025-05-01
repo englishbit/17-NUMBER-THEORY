b5977901f97bd96c6ceb2bdb2515089b3b9f1987
SK <englishenglishenglish@proton.me>
2025-05-01 15:32:22 +0000

    Add files via upload

---
+++ 3__Reversing a number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+
+    int num;
+    scanf("%d",&num);
+    int rem;
+    int rvrs_num=0;
+
+    while(num!=0){
+        rem=num%10;
+        rvrs_num=rvrs_num*10+rem;
+        num=num/10;
+        
+
+    }
+
+    printf("%d",rvrs_num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
