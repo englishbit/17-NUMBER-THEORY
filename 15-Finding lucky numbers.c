68ffd5d35794555d754ce7461bd19fdbd68dfad6
SK <englishenglishenglish@proton.me>
2025-06-28 14:22:24 +0000

    Update 15-Finding lucky numbers.c

--- 15-Finding lucky numbers.c
+++ 15-Finding lucky numbers.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 int main(){
+
+    //logic for taking input
     int n;
     scanf("%d",&n);
+
+    //logic for special case
     if(n<1){
         printf("Invalid input\n");
         return 1;
     }
 
+    
     int array[n+1];
 
+
+    //logic for filling the array 
     for(int i=1;i<=n;i++){
         array[i]=0;    
     }
 
+    //logic for finding the lucky numbers
     for(int i=1;i<=n;i++){
         if(array[i]==0){
             
@@ -23,6 +31,7 @@ int main(){
         
     }
 
+    //logic for output
     for(int i=1;i<=n;i++){
         if(array[i]==0){
             printf("%d ",i);
@@ -30,20 +39,6 @@ int main(){
     }
 
     printf("\n");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
